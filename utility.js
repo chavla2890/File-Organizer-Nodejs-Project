ac4fab6774b66c04435bbb7893fd59688b0463c1
AAKASH PRAKASHBHAI CHAVLA <akashchawla497@gmail.com>
2024-07-15 18:59:53 +0530

    Add new project guess the number  with README.md

---
+++ utility.js
@@ -0,0 +1,9 @@
+let utility = {}
+utility.types = {
+    media: ["mp4", "mkv"],
+    archives: ['zip', '7z', 'rar', 'tar', 'gz', 'ar', 'iso', 'xz'],
+    document: ['docx', 'doc', 'pdf', 'xlsx', 'xls', 'odt', 'ods', 'odp', 'odf', 'txt','ps', 'tex'],
+    app: ['exe', 'dmg', 'pkg', "deb"]
+}
+
+module.export = utility;
